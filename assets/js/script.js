c417474e9f695862d4c65829ba427477fad3a8a4
allwyns <allwyns@axioned.com>
2022-01-20 14:13:46 +0530

    add: onclick modal pop up event added in financial service section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,65 @@
 // *********************** home/index page script start ***********************
 // ============================================================================
 
-// ************************ header start ************************
-// ------------ fixed menu onscroll start ------------ 
-var primary_menu = document.querySelector(".primary_menu");
+page_check = document.querySelector("body > div");
 
-window.addEventListener('scroll', function(){
-    if(window.scrollY > 80){
-        primary_menu.classList.add("fixed_menu");
-    }
-    else{
-        primary_menu.classList.remove("fixed_menu");
-    }
-});
-// ------------ fixed menu onscroll end ------------ 
-// ************************ header end ************************
+if(page_check.className == "container home_page"){
+    // ************************ header fixed menu onscroll start ************************
+    var primary_menu = document.querySelector(".primary_menu");
 
-// ************************ main section start ************************
-// ------------ main slider end ------------ 
-$('.main_slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true,
-    prevArrow: $('.prev'),
-    nextArrow: $('.next')
-});
-// ------------ main slider end ------------ 
-// ************************* main section end *************************
+    window.addEventListener('scroll', function(){
+        if(window.scrollY > 80){
+            primary_menu.classList.add("fixed_menu");
+        }
+        else{
+            primary_menu.classList.remove("fixed_menu");
+        }
+    });
+    // ************************ header fixed menu onscroll end ************************
+
+    // ************************ main section slider start ************************
+    $('.main_slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
+    });
+    // ************************* main section slider end *************************
+    
+    // ********************* financial service section start *********************
+    // ------------ modal start ------------ 
+    var modal = document.querySelectorAll(".modal");
+    var overlay = document.querySelectorAll(".overlay");
+    var read_more_btn = document.querySelectorAll(".read_more_btn");
+    var service_content_box = document.querySelector("body");
+    
+    read_more_btn.forEach(function(e){
+        var index = Array.prototype.indexOf.call(read_more_btn, e);
+        e.addEventListener("click", function(){
+            window.scrollY = 1040;
+            modal[index].classList.remove("hide");
+    
+            var x=window.scrollX;
+            var y=window.scrollY;
+            window.onscroll = function(){ window.scrollTo(x, y); };
+        });
+        
+        overlay[index].addEventListener("click", function(){
+            modal[index].classList.add("hide");
+            window.onscroll = function(){};
+        });
+    });
+
+    // ------------ modal end ------------ 
+    // ********************** financial service section end **********************
+
+}
 
 // ============================================================================
 // *********************** home/index page script start ***********************
